0949231e43db66df4bf847ddc3fe610c0239a9f4
Jacob <program.acquaint533@passinbox.com>
2025-03-05 11:58:38 +1100

    Changed server to test api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/authRoutes"; 
 
 dotenv.config();
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
+// ✅ Mount API routes
+app.use("/api/auth", authRoutes);
+
+// ✅ Test route
+app.get("/test", (req, res) => {
+    res.send("API is working!");
+  });
 
 app.get("/", (req, res) => {
   res.send("TDAC API is running...");
