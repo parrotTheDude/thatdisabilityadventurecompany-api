19d718439aa3ab3098672a062608a2eb78189494
Jacob <program.acquaint533@passinbox.com>
2025-03-13 16:24:00 +1100

    Reorganized routes to avoid conflict

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -20,10 +20,10 @@ router.get("/me", protect, getUserProfile);
 router.get("/search", protect, searchUsers);
 router.get("/export", protect, exportUsers);
 router.get("/types", protect, getUserTypes);
+router.get("/genders", protect, getUserGenders);
+router.get("/subscriptions/lists", protect, getSubscriptionLists);
 router.get("/:id", protect, getUserById);
 router.post("/", protect, createUser);
-router.get("/genders", protect, getUserGenders);
 router.put("/:id", protect, updateUser);
-router.get("/subscriptions/lists", protect, getSubscriptionLists);
 
 export default router;
